7a8ca7f40ed509f067de1be16c79e75f2280f2bb
tumregels <lev@valital.com>
2020-07-15 12:21:21 -0400

    rename Serpent subdirectories

--- analysex/file_map.py
+++ analysex/file_map.py
@@ -20,25 +20,25 @@ file_map = {
     'ASSBLY_A': {
         'drag_res': 'Dragon/ASSBLY_A/output_2020-06-06_13-46-57/ASSBLY_CASEA.result',
         'drag_burn': 'Dragon/ASSBLY_A/output_2020-06-06_13-46-57/ASSBLY_CASEA_BURN2.txt',
-        'serp_dep': 'Serpent/ASSEMBLY_A/output_2020-05-13_12-24-33/ASSBLY_CASEA_mc_dep.mat',
-        'serp_res': 'Serpent/ASSEMBLY_A/output_2020-05-13_12-24-33/ASSBLY_CASEA_mc_res.mat',
+        'serp_dep': 'Serpent/ASSBLY_A/output_2020-05-13_12-24-33/ASSBLY_CASEA_mc_dep.mat',
+        'serp_res': 'Serpent/ASSBLY_A/output_2020-05-13_12-24-33/ASSBLY_CASEA_mc_res.mat',
     },
     'ASSBLY_B': {
         'drag_res': 'Dragon/ASSBLY_B/output_2020-06-06_13-45-45/ASSBLY_CASEB.result',
         'drag_burn': 'Dragon/ASSBLY_B/output_2020-06-06_13-45-45/ASSBLY_CASEB_BURN2.txt',
-        'serp_dep': 'Serpent/ASSEMBLY_B/output_2020-05-13_22-47-03/ASSBLY_CASEB_mc_dep.mat',
-        'serp_res': 'Serpent/ASSEMBLY_B/output_2020-05-13_22-47-03/ASSBLY_CASEB_mc_res.mat',
+        'serp_dep': 'Serpent/ASSBLY_B/output_2020-05-13_22-47-03/ASSBLY_CASEB_mc_dep.mat',
+        'serp_res': 'Serpent/ASSBLY_B/output_2020-05-13_22-47-03/ASSBLY_CASEB_mc_res.mat',
     },
     'ASSBLY_C': {
         'drag_res': 'Dragon/ASSBLY_C/output_2020-06-06_13-43-34/ASSBLY_CASEC.result',
         'drag_burn': 'Dragon/ASSBLY_C/output_2020-06-06_13-43-34/ASSBLY_CASEC_BURN2.txt',
-        'serp_dep': 'Serpent/ASSEMBLY_C/output_2020-05-18_15-13-07/ASSBLY_CASEC_mc_dep.mat',
-        'serp_res': 'Serpent/ASSEMBLY_C/output_2020-05-18_15-13-07/ASSBLY_CASEC_mc_res.mat',
+        'serp_dep': 'Serpent/ASSBLY_C/output_2020-05-18_15-13-07/ASSBLY_CASEC_mc_dep.mat',
+        'serp_res': 'Serpent/ASSBLY_C/output_2020-05-18_15-13-07/ASSBLY_CASEC_mc_res.mat',
     },
     'ASSBLY_D': {
         'drag_res': 'Dragon/ASSBLY_D/output_2020-07-06_17-12-24/ASSBLY_CASED.result',
         'drag_burn': 'Dragon/ASSBLY_D/output_2020-07-06_17-12-24/ASSBLY_CASED_BURN2.txt',
-        'serp_dep': 'Serpent/ASSEMBLY_D/output_2020-05-21_10-05-13/ASSBLY_CASED_mc_dep.mat',
-        'serp_res': 'Serpent/ASSEMBLY_D/output_2020-05-21_10-05-13/ASSBLY_CASED_mc_res.mat',
+        'serp_dep': 'Serpent/ASSBLY_D/output_2020-05-21_10-05-13/ASSBLY_CASED_mc_dep.mat',
+        'serp_res': 'Serpent/ASSBLY_D/output_2020-05-21_10-05-13/ASSBLY_CASED_mc_res.mat',
     },
 }
